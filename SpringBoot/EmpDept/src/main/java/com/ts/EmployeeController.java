99a789920d34694db80626fe3a782b83505ce89e
Ranjith Redddy <ranjithreddykondra@gmail.com>
2024-01-22 11:24:24 +0530

    SpringBoot EmpDept Project created

---
+++ SpringBoot/EmpDept/src/main/java/com/ts/EmployeeController.java
@@ -0,0 +1,12 @@
+package com.ts;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dao.EmployeeDao;
+@RestController
+public class EmployeeController {
+
+	@Autowired
+	EmployeeDao employeeDao;
+}
